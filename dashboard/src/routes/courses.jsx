afc89557c4e28eece2373d846fe012504411fad9
Soham Sinha <gha4my@virginia.edu>
2025-05-19 17:24:24 -0400

    Rough Template for how Course Page will look like

--- dashboard/src/routes/courses.jsx
+++ dashboard/src/routes/courses.jsx
@@ -1,14 +1,37 @@
 import Sidebar from '../components/Sidebar';
+import './courses.css';
 
 const Courses = () => {
   return (
     <div>
       <Sidebar />
-      <main>
+       <main>
         <h1>Courses</h1>
-        {/* Add your courses content here */}
-      </main>
+        {/*
+        <div class="course-dashboard">
+        <button class="add-class-btn">Add Class</button>
+          <div class="card"> Class 1 </div>
+        </div>
+        */}
+  <div class="column">
+    <div class="card">
+      <div class="container">
+        <h2>Jane Doe</h2>
+        <p class="title">Class 1</p>
+        <p>lorem ipsum ipsum lorem.</p>
+        <p>jane@example.com</p>
+        <p><button class="button">View Class</button></p>
+      </div>
     </div>
+  </div>
+        
+      </main>
+      
+
+    </div>
+    
+
+    
   );
 };
 
