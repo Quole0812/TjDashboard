552e4696b6e005bdf1946a37628d63879ddbbdd9
Shana Tran <vytran@ad.unc.edu>
2025-05-21 12:27:42 -0400

    Make home page dynamic

--- dashboard/src/components/DashboardWidget.jsx
+++ dashboard/src/components/DashboardWidget.jsx
@@ -1,15 +1,3 @@
-// import { Link } from 'react-router-dom';
-// import './DashboardWidget.css';
-
-// const DashboardWidget = ({ title, to }) => (
-//   <Link className="dashboard-widget" to={to}>
-//     <div className="dashboard-widget-content">
-//       <div className="dashboard-widget-title">{title}</div>
-//     </div>
-//   </Link>
-// );
-
-// export default DashboardWidget;
 import { Link } from 'react-router-dom';
 import './DashboardWidget.css';
 
