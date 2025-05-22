a238de5e95278226e0adfde2dd5399b5b2819fd5
SarathyS101 <se.sarathy17@gmail.com>
2025-05-22 14:46:28 -0400

    styling

--- dashboard/src/routes/studentDir.jsx
+++ dashboard/src/routes/studentDir.jsx
@@ -55,59 +55,61 @@ export default function StudentDirectory() {
 
   return (
     <>
-      <Sidebar />
-      <div className="layout">
-        <div className="content">
-          <div className="main-footer">
-            <div className="header-row">
-              <h1>Student Directory</h1>
-              <AddStudentToTJ fetchStudents={fetchStudents} />
+      <div className="page-wrapper">
+        <Sidebar />
+        <div className="layout">
+          <div className="content">
+            <div className="main-footer">
+              <div className="header-row">
+                <h1>Student Directory</h1>
+                <AddStudentToTJ fetchStudents={fetchStudents} />
+              </div>
             </div>
-          </div>
-          <div className="categories">
-            <div className="student-header">
-              <p className="header-cell">Name</p>
-              <p className="header-cell">ID</p>
-              <p className="header-cell">Grade</p>
-              <p className="header-cell">Actions</p>
+            <div className="categories">
+              <div className="student-header">
+                <p className="header-cell">Name</p>
+                <p className="header-cell">ID</p>
+                <p className="header-cell">Grade</p>
+                <p className="header-cell">Actions</p>
+              </div>
             </div>
-          </div>
 
-          <div className="scroll-container">
-            {!noStudents &&
-              students.map((student, i) => (
-                <div key={i} className="student-card student-entry">
-                  <p>{student.name}</p>
-                  <p>{student.id}</p>
-                  <p>{student.grade}</p>
+            <div className="scroll-container">
+              {!noStudents &&
+                students.map((student, i) => (
+                  <div key={i} className="student-card student-entry">
+                    <p>{student.name}</p>
+                    <p>{student.id}</p>
+                    <p>{student.grade}</p>
 
-                  <div className="student-actions">
-                    <div className="tooltip">
-                      <EditStudent
-                        currentName={student.name}
-                        currentGrade={student.grade}
-                        id={student.id}
-                        fetchStudents={fetchStudents}
-                      />
-                      <span className="tooltiptext">Edit Student</span>
-                    </div>
-                    <div className="tooltip">
-                      <button
-                        className="icon-button"
-                        onClick={() => deleteStudent(student.id)}
-                      >
-                        <VscError />
-                      </button>
-                      <span className="tooltiptext">Delete Student</span>
+                    <div className="student-actions">
+                      <div className="tooltip">
+                        <EditStudent
+                          currentName={student.name}
+                          currentGrade={student.grade}
+                          id={student.id}
+                          fetchStudents={fetchStudents}
+                        />
+                        <span className="tooltiptext">Edit Student</span>
+                      </div>
+                      <div className="tooltip">
+                        <button
+                          className="icon-button"
+                          onClick={() => deleteStudent(student.id)}
+                        >
+                          <VscError />
+                        </button>
+                        <span className="tooltiptext">Delete Student</span>
+                      </div>
                     </div>
                   </div>
+                ))}
+              {noStudents && (
+                <div className="no-students">
+                  <h2>No students found.</h2>
                 </div>
-              ))}
-            {noStudents && (
-              <div className="no-students">
-                <h2>No students found.</h2>
-              </div>
-            )}
+              )}
+            </div>
           </div>
         </div>
       </div>
